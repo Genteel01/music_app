a48f95d476dd13e0f0150ee3083cdfc39fad1351
George <georgeshepherd3@gmail.com>
2021-06-15 12:23:30 +1000

    Initial Commit

---
+++ music_player/DataModel.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DataModel extends ChangeNotifier {
+  DataModel()
+  {
+    fetch();
+  }
+
+  void fetch()
+  {
+
+  }
+
+}
---
+++ music_player/android/app/src/main/kotlin/au/com/genteel01/music_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package au.com.genteel01.music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
